02c616077509a2f04484115090f7dcb7c64af368
Joaogl <joaoguerralourenco@hotmail.com>
2016-01-15 12:34:42 +0000

    Inicial commit

---
+++ src/blogServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace jlourenco\blog;
+
+use Illuminate\Support\ServiceProvider;
+
+class blogServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publish our routes
+		require __DIR__ . '/routes.php';
+
+        // Publish our views
+        $this->loadViewsFrom(base_path("resources/views"), 'blog');
+        $this->publishes([
+            __DIR__ .  '/views' => base_path("resources/views")
+        ]);
+
+        // Publish our migrations
+        $this->publishes([
+            __DIR__ .  '/migrations' => base_path("database/migrations")
+        ], 'migrations');
+
+        // Publish a config file
+        $this->publishes([
+            __DIR__ . '/config' => base_path('/config')
+        ], 'config');
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Bind the
+        $this->app->bind('blog', function(){
+            return new blog;
+        });
+    }
+}
