876046552565223b1d1e56adaf585ba5b570962d
Joaogl <joaoguerralourenco@hotmail.com>
2016-01-18 12:46:39 +0000

    Updated migrations

---
+++ src/Migrations/2016_01_15_123600_create_blog_tables.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateBlogTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('BlogCategory', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('description', 250);
+
+            $table->timestamps();
+            $table->softDeletes();
+            $table->creation();
+        });
+
+        Schema::create('BlogPost', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100);
+            $table->text('contents');
+            $table->integer('category')->unsigned();
+            $table->integer('author')->unsigned();
+            $table->integer('likes')->default(0);
+            $table->integer('shares')->default(0);
+            $table->integer('views')->default(0);
+            $table->string('keywords', 250);
+
+            $table->timestamps();
+            $table->softDeletes();
+            $table->creation();
+
+            $table->foreign('category')->references('id')->on('BlogCategory');
+            $table->foreign('author')->references('id')->on('User');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::drop('BlogPost');
+        Schema::drop('BlogCategory');
+
+    }
+}
